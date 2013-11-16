a7a30c2e66bde5da30836879d6f0aa5e89be00bb
Eric Meadows-Jonsson <eric.meadows.jonsson@gmail.com>
2013-11-16 23:21:44 +0100

    Add conversion to decimal

--- lib/decimal.ex
+++ lib/decimal.ex
@@ -1,2 +1,67 @@
 defmodule Decimal do
+  defrecordp :d, __MODULE__, [coef: 0, exp: 0]
+
+  def to_decimal(d() = d), do: d
+  def to_decimal(int) when is_integer(int), do: d(coef: int)
+  def to_decimal(float) when is_float(float), do: to_decimal(float_to_binary(float))
+  def to_decimal(binary) when is_binary(binary), do: parse(binary)
+
+  defp parse("NaN") do
+    d(coef: :NaN)
+  end
+
+  defp parse("+" <> bin) do
+    parse_unsign(bin)
+  end
+
+  defp parse("-" <> bin) do
+    d(coef: coef) = d = parse_unsign(bin)
+    d(d, coef: -coef)
+  end
+
+  defp parse(bin) do
+    parse_unsign(bin)
+  end
+
+  defp parse_unsign("inf") do
+    d(coef: :inf)
+  end
+
+  defp parse_unsign(bin) do
+    { int, rest } = parse_digits(bin)
+    { float, rest } = parse_float(rest)
+    { exp, rest } = parse_exp(rest)
+
+    if int == [] or rest != "", do: raise ArgumentError
+    if exp == [], do: exp = '0'
+
+    d(coef: list_to_integer(int ++ float), exp: list_to_integer(exp) - length(float))
+  end
+
+  defp parse_float("." <> rest), do: parse_digits(rest)
+  defp parse_float(bin), do: { [], bin }
+
+  defp parse_exp(<< e, rest :: binary >>) when e in [?e, ?E] do
+    case rest do
+      << sign, rest :: binary >> when sign in [?+, ?-] ->
+        { digits, rest } = parse_digits(rest)
+        { [sign|digits], rest }
+      _ ->
+        parse_digits(rest)
+    end
+  end
+
+  defp parse_exp(bin) do
+    { [], bin }
+  end
+
+  defp parse_digits(bin), do: parse_digits(bin, [])
+
+  defp parse_digits(<< digit, rest :: binary >>, acc) when digit in ?0..?9 do
+    parse_digits(rest, [digit|acc])
+  end
+
+  defp parse_digits(rest, acc) do
+    { :lists.reverse(acc), rest }
+  end
 end
--- test/decimal_test.exs
+++ test/decimal_test.exs
@@ -1,7 +1,46 @@
 defmodule DecimalTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert(true)
+  defrecordp :d, Decimal, [coef: 0, exp: 0]
+
+  test "basic conversion" do
+    assert Decimal.to_decimal(d(coef: 0, exp: 0)) == d(coef: 0, exp: 0)
+    assert Decimal.to_decimal(123) == d(coef: 123, exp: 0)
+  end
+
+  test "float conversion" do
+    assert Decimal.to_decimal(123.0) == d(coef: 123000000000000000000, exp: -18)
+    assert Decimal.to_decimal(1.5) == d(coef: 150000000000000000000, exp: -20)
+  end
+
+  test "string conversion" do
+    assert Decimal.to_decimal("123") == d(coef: 123, exp: 0)
+    assert Decimal.to_decimal("+123") == d(coef: 123, exp: 0)
+    assert Decimal.to_decimal("-123") == d(coef: -123, exp: 0)
+
+    assert Decimal.to_decimal("123.0") == d(coef: 1230, exp: -1)
+    assert Decimal.to_decimal("+123.0") == d(coef: 1230, exp: -1)
+    assert Decimal.to_decimal("-123.0") == d(coef: -1230, exp: -1)
+
+    assert Decimal.to_decimal("1.5") == d(coef: 15, exp: -1)
+    assert Decimal.to_decimal("+1.5") == d(coef: 15, exp: -1)
+    assert Decimal.to_decimal("-1.5") == d(coef: -15, exp: -1)
+
+    assert Decimal.to_decimal("0") == d(coef: 0, exp: 0)
+    assert Decimal.to_decimal("+0") == d(coef: 0, exp: 0)
+    assert Decimal.to_decimal("-0") == d(coef: 0, exp: 0)
+
+    assert Decimal.to_decimal("1230e13") == d(coef: 1230, exp: 13)
+    assert Decimal.to_decimal("+1230e+2") == d(coef: 1230, exp: 2)
+    assert Decimal.to_decimal("-1230e-2") == d(coef: -1230, exp: -2)
+
+
+    assert Decimal.to_decimal("1230.00e13") == d(coef: 123000, exp: 11)
+    assert Decimal.to_decimal("+1230.1230e+5") == d(coef: 12301230, exp: 1)
+    assert Decimal.to_decimal("-1230.01010e-5") == d(coef: -123001010, exp: -10)
+
+    assert Decimal.to_decimal("0e0") == d(coef: 0, exp: 0)
+    assert Decimal.to_decimal("+0e-0") == d(coef: 0, exp: 0)
+    assert Decimal.to_decimal("-0e+0") == d(coef: 0, exp: 0)
   end
 end
