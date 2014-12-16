88087a35f0bfcdb120d28ec5527c77edc2d00bc7
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2014-12-16 23:57:50 +0100

    Release v1.0.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Decimal.Mixfile do
 
   def project do
     [app: :decimal,
-     version: "1.0.1-dev",
+     version: "1.0.1",
      elixir: "~> 1.0",
      deps: deps,
      name: "Decimal",
