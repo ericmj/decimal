d3a7c35e0f8b06689f7f359eec17f4f8702896ad
Kip Cole <kipcole9@gmail.com>
2017-07-09 06:56:29 +0900

    Call Integer.to_char_list for Elixir versions < 1.3.0 and Integer.to_charlist for Elixir versions >= 1.3.0 (#51)

--- lib/decimal.ex
+++ lib/decimal.ex
@@ -941,7 +941,7 @@ defmodule Decimal do
   end
 
   def to_string(%Decimal{sign: sign, coef: coef, exp: exp}, :normal) do
-    list = Integer.to_char_list(coef)
+    list = integer_to_charlist(coef)
 
     list =
       if exp >= 0 do
@@ -960,7 +960,7 @@ defmodule Decimal do
   end
 
   def to_string(%Decimal{sign: sign, coef: coef, exp: exp}, :scientific) do
-    list = Integer.to_char_list(coef)
+    list = integer_to_charlist(coef)
     length = length(list)
     adjusted = exp + length - 1
 
@@ -983,7 +983,7 @@ defmodule Decimal do
           list = if length > 1, do: List.insert_at(list, 1, ?.), else: list
           list = list ++ 'E'
           list = if exp >= 0, do: list ++ '+', else: list
-          list ++ Integer.to_char_list(adjusted)
+          list ++ integer_to_charlist(adjusted)
       end
 
     list = if sign == -1, do: [?-|list], else: list
@@ -1443,7 +1443,14 @@ defmodule Decimal do
       {:ok, result}
     end
   end
+
+  if Version.compare(System.version, "1.3.0") == :lt do
+    defp integer_to_charlist(string), do: Integer.to_char_list(string)
+  else
+    defp integer_to_charlist(string), do: Integer.to_charlist(string)
+  end
 end
+
 
 defimpl Inspect, for: Decimal do
   def inspect(dec, _opts) do
