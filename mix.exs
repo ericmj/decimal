b7256f092f5596bbd9187afc99918fbd16d7ddd9
Wojtek Mach <wojtek@wojtekmach.pl>
2018-11-24 00:26:53 +0100

    ex_doc already depends on earmark

--- mix.exs
+++ mix.exs
@@ -23,8 +23,7 @@ defmodule Decimal.Mixfile do
 
   defp deps() do
     [
-      {:ex_doc, ">= 0.0.0", only: :dev},
-      {:earmark, ">= 0.0.0", only: :dev}
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
 
