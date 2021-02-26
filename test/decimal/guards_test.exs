abb40d34d200c228a1a5dca95dc3d7ac8b77e6e2
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2021-02-26 06:48:49 +0100

    Same surgery for the test

--- test/decimal/guards_test.exs
+++ test/decimal/guards_test.exs
@@ -1,11 +1,12 @@
-if String.to_integer(System.otp_release()) > 22 and
-     Version.compare(System.version(), "1.11.0") != :lt do
-  defmodule DecimalGuardsTest do
-    use ExUnit.Case, async: true
+if Version.compare(System.version(), "1.11.0") != :lt do
+  if String.to_integer(apply(System, :otp_release, [])) > 22 do
+    defmodule DecimalGuardsTest do
+      use ExUnit.Case, async: true
 
-    require Decimal
-    import Decimal.Guards
+      require Decimal
+      import Decimal.Guards
 
-    doctest Decimal.Guards
+      doctest Decimal.Guards
+    end
   end
 end
